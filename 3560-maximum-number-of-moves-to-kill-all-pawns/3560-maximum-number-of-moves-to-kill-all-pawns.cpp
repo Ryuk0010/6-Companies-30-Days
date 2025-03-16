b1c07b7c783ec1a9ca1260c960a39f4fdfb96218
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-03-17 02:21:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3560-maximum-number-of-moves-to-kill-all-pawns/3560-maximum-number-of-moves-to-kill-all-pawns.cpp
+++ 3560-maximum-number-of-moves-to-kill-all-pawns/3560-maximum-number-of-moves-to-kill-all-pawns.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> directions = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
+    void BFS(int x, int y, int idx, vector<vector<int>>& minDist, vector<vector<int>> ps){
 
-    void BFS(int x, int y, int index, vector<vector<int>>& minDist, vector<vector<int>>& pos) {
-        vector<vector<int>> t(50, vector<int>(50, -1));
-        queue<pair<int, int>> que;
-        que.push({x, y});
-        t[x][y] = 0;
+        vector<vector<int>> vis(50, vector<int>(50, -1));
+        queue<pair<int, int>> q;
+        q.push({x, y});
+        vis[x][y] = 0;
 
-        while(!que.empty()) {
-            auto[currX, currY] = que.front();
-            que.pop();
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int currentX = it.first;
+            int currentY = it.second;
 
-            for(auto &dir : directions) {
-                int newX = currX + dir[0];
-                int newY = currY + dir[1];
-
-                if(newX >= 0 && newX < 50 && newY >= 0 && newY < 50 && t[newX][newY] == -1) {
-                    t[newX][newY] = t[currX][currY] + 1;
-                    que.push({newX, newY});
+            for(auto dir: directions){
+                int newX = currentX + dir[0];
+                int newY = currentY + dir[1];
+                
+                if(newX >= 0 && newX < 50 && newY >= 0 && newY < 50 && vis[newX][newY] == -1){
+                    q.push({newX, newY});
+                    vis[newX][newY] = 1 + vis[currentX][currentY];
                 }
             }
         }
-
-        for(int i = 0; i < pos.size(); i++) {
-            int x_ = pos[i][0];
-            int y_ = pos[i][1];
-
-            minDist[index][i] = t[x_][y_];
+        for(int i = 0; i < ps.size(); i++){
+            int x_ = ps[i][0];
+            int y_ = ps[i][1];
+            minDist[idx][i] = vis[x_][y_];
         }
     }
 
-    int solve(vector<vector<int>>& minDist, int idx, int mask, int n, bool Alice, int dp[51][1<<15]) {
+    int solve(vector<vector<int>>& minDist, int idx, int mask, int n, bool Alice,int dp[51][1 << 15]) {
         if(mask == 0) {
-            return 0; //no more moves/steps required
+            return 0; 
         }
 
-        if(dp[idx][mask] != -1) {
-            return dp[idx][mask];
-        }
-
+        if(dp[idx][mask] != -1) return dp[idx][mask];
         int result = (Alice == true) ? -1 : 1e9;
 
         for(int i = 1; i < n; i++) {
@@ -47,7 +44,7 @@ public:
                 int moves = minDist[idx][i];
 
                 if(Alice == true) {
-                    result = max(result, moves + solve(minDist, i, mask ^ (1 << (i-1)), n, !Alice, dp));
+                    result = max(result, moves + solve(minDist, i, mask ^ (1 << (i-1)), n, !Alice ,dp));
                 } else {
                     result = min(result, moves + solve(minDist, i, mask ^ (1 << (i-1)), n, !Alice, dp));
                 }
@@ -66,7 +63,7 @@ public:
             pos.push_back({vec[0], vec[1]});
         }
 
-        //minDist[][]
+        
         vector<vector<int>> minDist(n+1, vector<int>(n+1, 0));
 
         for(int i = 0; i < pos.size(); i++) {
@@ -76,8 +73,7 @@ public:
         }
 
 
-        //call solve
-        //0th index par knight hai mera in pos array
+        
         bool Alice = true;
         int dp[51][1 << 15];
         memset(dp, -1, sizeof(dp));
