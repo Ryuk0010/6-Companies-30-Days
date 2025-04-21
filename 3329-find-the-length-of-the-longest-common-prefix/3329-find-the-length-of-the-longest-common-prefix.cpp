89d3dbe4c9578cd7577a7289f6ccb277a2b3f74b
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-04-21 10:16:14 +0530

    Time: 123 ms (94.3%), Space: 160.7 MB (59.11%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
-    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_map<string, int> mpp;
-        for(auto it: arr1){
-            string conStr = to_string(it);
-            string str = "";
-            for(auto i: conStr){
-                str += i;
-                mpp[str]++; 
+    class TrieNode{
+    public:
+        TrieNode *children[10];
+        TrieNode(){
+            for(int i = 0; i < 10; i++){
+                children[i] = nullptr;
             }
         }
-        int ans = 0;
-        for(auto it: arr2){
-            string conStr = to_string(it);
-            string str = "";
-            for(auto i: conStr){
-                str += i;
-                if(mpp.find(str) != mpp.end()){
-                    ans = max(ans, static_cast<int>(str.length()));
-                }
+    };
+    TrieNode *root;
+    
+    void makeTrie(string str){
+        TrieNode *crl = root;
+        for(int i = 0; i < str.size(); i++){
+            int num = str[i] - '0';
+            if(crl->children[num] == nullptr){
+                crl->children[num] = new TrieNode();
             }
+            crl = crl->children[num];
+        }
+    }
+
+    int searchTrie(string str){
+        TrieNode *crl = root;
+        int sz = 0;
+        for(int i = 0; i < str.size(); i++){
+            int num = str[i] - '0';
+            if(crl->children[num] == nullptr) return sz;
+            sz++;
+            crl = crl->children[num];
+        }
+        return sz;
+    }
+    
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        root = new TrieNode();
+        int ans = 0;
+        for(int i = 0; i < arr1.size(); i++){
+            string str = to_string(arr1[i]);
+            makeTrie(str);
+        }
+        for(int i = 0; i < arr2.size(); i++){
+            string str = to_string(arr2[i]);
+            
+            ans = max(ans, searchTrie(str));
         }
         return ans;
     }
